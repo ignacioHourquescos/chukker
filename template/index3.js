b31b959e7887d89837c9da93995d12896b0e4df9
jimancio <ihourquescos@gmail.com>
2020-12-09 11:22:10 -0300

    cambios del 9

--- template/index3.js
+++ template/index3.js
@@ -1,15 +1,27 @@
 
 function agregarfila(){
- var table = document.getElementById("tabla");
+ var table = document.getElementById("tabla_remate");
  var row = table.insertRow(1);
  var cell1 = row.insertCell(0);
  var cell2 = row.insertCell(1);
  var cell3 = row.insertCell(2);
  var cell4 = row.insertCell(3);
  cell1.innerHTML = "478";
- cell2.innerHTML = "Fernando";
+ cell2.innerHTML = "Facundo Cabral";
  cell3.innerHTML = "MOntevideo, Uru";
- cell4.innerHTML = "$14500";
+ cell4.innerHTML = "$"+document.getElementById("input_oferta").value;
+ swal("Su Oferta por "+cell4.innerHTML + " se realizo con exito!");
+ window.scrollTo(0,0);
+ document.getElementById("ofertar").style.zIndex="0";
+
+ if (window.matchMedia("(max-width: 776px)").matches) {
+   document.getElementById("pedigree").style.display="block";
+} else {
+	document.getElementById("video").style.display="block";
+}
+document.getElementById("preoferta").innerHTML=document.getElementById("input_oferta").value;
+ document.getElementById("tabla").style.display="none";
+ 
 
 }
 
@@ -18,19 +30,41 @@ function agregarfila(){
     document.getElementById("video").style.display="block";
     document.getElementById("pedigree").style.display="none";
     document.getElementById("tabla").style.display="none";
-
+    document.getElementById("ofertar").style.display="block";
  }
 
  function mostrarPedigree(){
     document.getElementById("video").style.display="none";
     document.getElementById("tabla").style.display="none";
     document.getElementById("pedigree").style.display="block";
-
+    document.getElementById("ofertar").style.display="block";
+    window.scrollTo(0, 1000);
  }
 
 
  function mostrarOfertas(){
    document.getElementById("video").style.display="none";
    document.getElementById("pedigree").style.display="none";
-   document.getElementById("tabla").style.display="block";
+   document.getElementById("tabla").style.display="flex"
+   document.getElementById("ofertar").style.zIndex="-10";
+   window.scrollTo(0, 1000);
 }
+
+
+function mostrarLogin(){
+   document.getElementById("video").style.display="none";
+   document.getElementById("pedigree").style.display="none";
+   document.getElementById("tabla").style.display="none";
+   document.getElementById("ofertar").style.display="block";
+
+}
+
+
+
+function on() {
+   document.getElementById("overlay").style.display = "block";
+ }
+ 
+ function off() {
+   document.getElementById("overlay").style.display = "none";
+ }
